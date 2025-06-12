2cb6ac08bb592a69158ad8ed3fa5a3029a988608
abrosxd <154523512+abrosxd@users.noreply.github.com>
2025-06-12 22:54:38 +0200

    Fix 1.2 | 12/06/2025

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://bigspaces.ru', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT!);
 }
 bootstrap();
